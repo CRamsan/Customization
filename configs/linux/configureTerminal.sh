0d693c8c87288b28b4b066d1b6c6f0b42451be5c
Cesar Ramirez <me@cramsan.com>
2017-04-18 08:43:10 +0000

    Adding bashrc and bash_profile files

--- configs/linux/configureTerminal.sh
+++ configs/linux/configureTerminal.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-local CWD=$(pwd)
-local CURRENT_CONFIG_FOLDER=$CWD/$CONFIG_FOLDER/$PLAT_CONFIG_LINUX/
-ln -s $CURRENT_CONFIG_FOLDER/inputrc ~/.inputrc
-ln -s $CURRENC_CONFIG_FOLDER/vimrc ~/.vimrc
-
+local LOCAL_CWD=$(pwd)
+local LOCAL_CURRENT_CONFIG_FOLDER=$LOCAL_CWD/$CONFIG_FOLDER/$PLAT_CONFIG_LINUX/
+ln -s $LOCAL_CURRENT_CONFIG_FOLDER/inputrc ~/.inputrc
+ln -s $LOCAL_CURRENT_CONFIG_FOLDER/vimrc ~/.vimrc
+ln -s $LOCAL_CURRENT_CONFIG_FOLDER/bashrc ~/.bashrc
+ln -s $LOCAL_CURRENT_CONFIG_FOLDER/bash_profile ~/.bash_profile
