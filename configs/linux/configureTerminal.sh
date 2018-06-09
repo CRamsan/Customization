9c2339af37f5541f7d152f5c334167237571deb4
Cesar Ramirez <me@cramsan.com>
2018-06-08 19:06:36 -0700

    Changes to better support git bash

--- configs/linux/configureTerminal.sh
+++ configs/linux/configureTerminal.sh
@@ -7,9 +7,11 @@ fi
 
 local LOCAL_CWD=$(pwd)
 local LOCAL_CURRENT_CONFIG_FOLDER=$LOCAL_CWD/$CONFIG_FOLDER/$PLAT_CONFIG_LINUX
+set -x
 ln -s $LOCAL_CURRENT_CONFIG_FOLDER/inputrc $HOME/.inputrc
 ln -s $LOCAL_CURRENT_CONFIG_FOLDER/vimrc $HOME/.vimrc
 ln -s $LOCAL_CURRENT_CONFIG_FOLDER/bashrc $HOME/.bashrc
 ln -s $LOCAL_CURRENT_CONFIG_FOLDER/bash_profile $HOME/.bash_profile
+set +x
 cp -n $LOCAL_CURRENT_CONFIG_FOLDER/bashrc_override $HOME/.bashrc_override
 cp -n $LOCAL_CURRENT_CONFIG_FOLDER/bash_profile_override $HOME/.bash_profile_override
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 . utils.sh # Source the file with helper functions
-
 L_PLATFORM='unknown'
 L_PLAT=`uname`
-if [ "$L_PLAT" == 'Linux' ]; then
+if [ "$L_PLAT" == 'Linux' ] || [ "$L_PLAT" == 'MINGW64_NT-10.0' ]; then
   L_PLATFORM=$PLAT_NAME_LINUX
 fi
-
 case "$L_PLATFORM" in
   "$PLAT_NAME_LINUX")
     run_plat_config $PLAT_CONFIG_LINUX
     ;;
   *)
-    echo "Unkown platform $PLAT, not deploying any changes"
+    echo "Unkown platform $L_PLAT, not deploying any changes"
     exit 2
 esac
